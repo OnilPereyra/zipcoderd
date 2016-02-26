08189cff0a67abf24aa86fe583f3e6c15b14435c
cpmproto <dewin.martinez@gmail.com>
2016-02-26 16:54:01 -0400

    Seach zipCode by name

--- README.md
+++ README.md
@@ -8,12 +8,18 @@ Install using npm:
     npm install zipcoderd
 
 ```javascript
-var zipcode = require('zipcode');
+var zipcode = require('zipcoderd');
 
 // returns data in [sector, province] format
 zipcode.lookup('10205'); // ['GAZCUE','SANTO DOMINGO']
 ```
+## Search by the name of the municipality and province
+```javascript
+var zipcode = require('zipcoderd');
 
+//Return Zipcode
+zipcode.seachByName('GAZCUE, SANTO DOMINGO');
+```
 ## License
 
 MIT
--- index.js
+++ index.js
@@ -2,11 +2,18 @@ var fs = require('fs');
 var trie = null;
 
 /**
+ * Get data by lines 
+ */
+function getLine(){
+  var data = fs.readFileSync(__dirname + '/zip_codes.csv', 'utf8');
+  return data.split('\r\n');
+};
+
+/**
  * Parses zip_codes.csv and creates a trie for fast lookups
  */
-function loadData() {
-  var data = fs.readFileSync(__dirname + '/zip_codes.csv', 'utf8');
-  var lines = data.split('\r\n');
+function loadData() {  
+  var lines = getLine();
   var trie = {};
 
   lines.forEach(function(line) {
@@ -17,10 +24,12 @@ function loadData() {
     for (var i = 0; i < zip.length; i++) {
       var num = zip[i];
       var pos = node[num];
-      if (pos == null)
+      if (pos == null){
         node = node[num] = (i === zip.length - 1) ? [sector, province] : {};
-      else
+      }
+      else{
         node = node[num];
+      }
     }
   });
 
@@ -41,9 +50,35 @@ exports.lookup = function(zip) {
   var node = trie;
   for (var i = 0; i < zip.length; i++) {
     var node = node[zip[i]];
-    if (node == null)
+    if (node == null){
       return null;
+    }
   }
 
   return Array.isArray(node) ? node : null;
+};
+
+
+/**
+ * Search by the name of the municipality and province
+ * Return: Zipcode
+ */
+exports.seachByName = function(name) {
+  var lines = getLine();
+  var value = null; 
+
+  if(!name){
+    return value;
+  }   
+
+  lines.forEach(function(line) {
+    var parts = line.split(',');
+    var zip = parts[0], sector = parts[1], province = parts[2];
+  
+    if(name.split(' ').join('').toUpperCase() === (sector +','+ province).split(' ').join('') ){
+      value = zip;
+      return;
+    }  
+  });  
+  return value;
 };
